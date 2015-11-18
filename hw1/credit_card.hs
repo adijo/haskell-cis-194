9c97dcbcc2ae65a4a0b75858f03de5960ddc7df0
Aditya Joshi <1adityajoshi@gmail.com>
2015-11-18 23:35:41 +0530

    added first three exercises

--- hw1/credit_card.hs
+++ hw1/credit_card.hs
@@ -8,3 +8,21 @@ revList (x : xs) = (revList xs) ++ [x]
 
 toDigits :: Integer -> [Integer]
 toDigits n = revList (toDigitsRev n)
+
+doubleEveryOtherHelper :: [Integer] -> [Integer]
+doubleEveryOtherHelper [] = []
+doubleEveryOtherHelper (x : []) = [2 * x]
+doubleEveryOtherHelper (x : y : xs) = [2 * x] ++ [y] ++ (doubleEveryOtherHelper xs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (x : xs) =
+		 if length xs `mod` 2 == 1
+		    then (doubleEveryOtherHelper (x : xs))
+		    else x : (doubleEveryOtherHelper xs)
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x : []) = foldl (+) 0 (toDigits(x))
+sumDigits (x : xs) = sumDigits(toDigits x) + sumDigits(xs)
+
